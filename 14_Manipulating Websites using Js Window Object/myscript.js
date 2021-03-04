5b1d9660d24f61816ea0068a45b5ba5c1a2be876
dhruba-roka <dhruba.roka123@gmail.com>
2021-03-04 17:15:53 +0545

    window object is added.

---
+++ 14_Manipulating Websites using Js Window Object/myscript.js
@@ -0,0 +1,16 @@
+console.log("THis is code with harry. ");
+let a = window;
+
+//window.alert("hello, this is dhruba testing");
+// a = prompt("This will destroy your computer. Type your name:  ");
+// a = confirm("Are you sure you want to delete this page?");
+
+a = window.innerHeight;
+a = innerWidth;//window nalekhda ni hune since it is global type
+a = scrollX;
+a = scrollY;
+a = location;
+
+a = window.history;
+
+console.log(a);
